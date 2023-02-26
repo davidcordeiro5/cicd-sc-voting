0cc35ce44d37cb0d1d63659851cb3a7f3d637f63
david_c <david.cordeiro@epitech.eu>
2023-02-26 17:22:39 +0100

    [add] describe to tests

--- test/voting.test.js
+++ test/voting.test.js
@@ -19,204 +19,207 @@ contract("Voting", function (accounts) {
   const VotingSessionNotStarted = "Voting session havent started yet.";
   const desc = "My proposal !";
 
-  // expectRevert(contractIstance.getVoter(addr, {from}), "")
-
-  // expectEvent(tx, "eventName", {param: ""})
-
   beforeEach(async () => {
     contractIstance = await Voting.new();
   });
 
-  it("should revert getter because onlyVoters modifier is used", async () => {
-    await expectRevert(
-      contractIstance.getVoter(owner, { from: owner }),
-      notVoter
-    );
+  describe("Getters", () => {
+    it("should revert getter because onlyVoters modifier is used", async () => {
+      await expectRevert(
+        contractIstance.getVoter(owner, { from: owner }),
+        notVoter
+      );
 
-    await expectRevert(
-      contractIstance.getOneProposal(0, { from: owner }),
-      notVoter
-    );
+      await expectRevert(
+        contractIstance.getOneProposal(0, { from: owner }),
+        notVoter
+      );
+    });
   });
 
-  it("should add voter", async () => {
-    await expectRevert(
-      contractIstance.addVoter(owner, { from: account1 }),
-      notOwner
-    );
+  describe("Setters", () => {
+    it("should add voter", async () => {
+      await expectRevert(
+        contractIstance.addVoter(owner, { from: account1 }),
+        notOwner
+      );
 
-    const tx = await contractIstance.addVoter(owner);
-    await expectRevert(contractIstance.addVoter(owner), "Already registered");
-    expectEvent(tx, "VoterRegistered");
+      const tx = await contractIstance.addVoter(owner);
+      await expectRevert(contractIstance.addVoter(owner), "Already registered");
+      expectEvent(tx, "VoterRegistered");
 
-    const voter = await contractIstance.getVoter(owner);
-    expect(voter.isRegistered).to.equal(true);
+      const voter = await contractIstance.getVoter(owner);
+      expect(voter.isRegistered).to.equal(true);
 
-    await contractIstance.startProposalsRegistering();
+      await contractIstance.startProposalsRegistering();
 
-    await expectRevert(
-      contractIstance.addVoter(owner),
-      "Voters registration is not open yet"
-    );
+      await expectRevert(
+        contractIstance.addVoter(owner),
+        "Voters registration is not open yet"
+      );
+    });
+
+    it("should add proposal", async () => {
+      await expectRevert(contractIstance.addProposal(""), notVoter);
+      await contractIstance.addVoter(owner);
+      await expectRevert(
+        contractIstance.addProposal(""),
+        "Proposals are not allowed yet"
+      );
+
+      await contractIstance.startProposalsRegistering();
+
+      await expectRevert(
+        contractIstance.addProposal(""),
+        "Vous ne pouvez pas ne rien proposer"
+      );
+
+      const tx = await contractIstance.addProposal(desc);
+
+      await expectEvent(tx, "ProposalRegistered");
+
+      const proposal = await contractIstance.getOneProposal(1);
+
+      expect(proposal.description).to.equal(desc);
+    });
+
+    it("should set a vote", async () => {
+      await expectRevert(contractIstance.setVote(0), notVoter);
+
+      await contractIstance.addVoter(owner);
+      await expectRevert(contractIstance.setVote(0), VotingSessionNotStarted);
+
+      await contractIstance.startProposalsRegistering();
+      await contractIstance.addProposal(desc);
+      await contractIstance.endProposalsRegistering();
+
+      await contractIstance.startVotingSession();
+
+      await expectRevert(contractIstance.setVote(42), "Proposal not found");
+
+      let proposal = await contractIstance.getOneProposal(1);
+      expect(proposal.voteCount).to.be.bignumber.equal(zero);
+
+      const tx = await contractIstance.setVote(1);
+
+      proposal = await contractIstance.getOneProposal(1);
+      expect(proposal.voteCount).to.be.bignumber.equal(one);
+
+      await expectEvent(tx, "Voted");
+    });
   });
 
-  it("should add proposal", async () => {
-    await expectRevert(contractIstance.addProposal(""), notVoter);
-    await contractIstance.addVoter(owner);
-    await expectRevert(
-      contractIstance.addProposal(""),
-      "Proposals are not allowed yet"
-    );
+  describe("Workflow status changes", () => {
+    it("should change work flow to start proposals registering", async () => {
+      await contractIstance.addVoter(owner);
+      await expectRevert(
+        contractIstance.startProposalsRegistering({
+          from: account1,
+        }),
+        notOwner
+      );
 
-    await contractIstance.startProposalsRegistering();
+      const tx = await contractIstance.startProposalsRegistering();
 
-    await expectRevert(
-      contractIstance.addProposal(""),
-      "Vous ne pouvez pas ne rien proposer"
-    );
+      await expectRevert(
+        contractIstance.startProposalsRegistering(),
+        "Registering proposals cant be started now"
+      );
 
-    const tx = await contractIstance.addProposal(desc);
+      expectEvent(tx, "WorkflowStatusChange");
 
-    await expectEvent(tx, "ProposalRegistered");
+      const proposal = await contractIstance.getOneProposal(0);
 
-    const proposal = await contractIstance.getOneProposal(1);
+      expect(proposal.description).to.equal("GENESIS");
+    });
 
-    expect(proposal.description).to.equal(desc);
+    it("should change work flow to end proposals registering", async () => {
+      await expectRevert(
+        contractIstance.endProposalsRegistering({
+          from: account1,
+        }),
+        notOwner
+      );
+
+      await expectRevert(
+        contractIstance.endProposalsRegistering(),
+        "Registering proposals havent started yet"
+      );
+
+      await contractIstance.startProposalsRegistering();
+      const tx = await contractIstance.endProposalsRegistering();
+
+      expectEvent(tx, "WorkflowStatusChange");
+    });
+
+    it("should change work flow to start voting session", async () => {
+      await expectRevert(
+        contractIstance.startVotingSession({
+          from: account1,
+        }),
+        notOwner
+      );
+
+      await expectRevert(
+        contractIstance.startVotingSession(),
+        "Registering proposals phase is not finished."
+      );
+
+      await contractIstance.startProposalsRegistering();
+      await contractIstance.endProposalsRegistering();
+
+      const tx = await contractIstance.startVotingSession();
+      expectEvent(tx, "WorkflowStatusChange");
+    });
+
+    it("should change work flow to end voting session", async () => {
+      await expectRevert(
+        contractIstance.endVotingSession({
+          from: account1,
+        }),
+        notOwner
+      );
+
+      await expectRevert(
+        contractIstance.endVotingSession(),
+        VotingSessionNotStarted
+      );
+
+      await contractIstance.startProposalsRegistering();
+      await contractIstance.endProposalsRegistering();
+      await contractIstance.startVotingSession();
+
+      const tx = await contractIstance.endVotingSession();
+      expectEvent(tx, "WorkflowStatusChange");
+    });
   });
 
-  it("should set a vote", async () => {
-    await expectRevert(contractIstance.setVote(0), notVoter);
+  describe("should tally votes", () => {
+    it("should tally votes and check winner before call the tallyVotes()", async () => {
+      await expectRevert(
+        contractIstance.tallyVotes({ from: account1 }),
+        notOwner
+      );
+      await expectRevert(
+        contractIstance.tallyVotes(),
+        "Current status is not voting session ended"
+      );
 
-    await contractIstance.addVoter(owner);
-    await expectRevert(contractIstance.setVote(0), VotingSessionNotStarted);
+      let winner = (await contractIstance.winningProposalID.call()).words[0];
+      expect(BN(winner)).to.be.bignumber.equal(zero);
 
-    await contractIstance.startProposalsRegistering();
-    await contractIstance.addProposal(desc);
-    await contractIstance.endProposalsRegistering();
+      await contractIstance.addVoter(owner);
+      await contractIstance.startProposalsRegistering();
+      await contractIstance.addProposal(desc);
+      await contractIstance.endProposalsRegistering();
+      await contractIstance.startVotingSession();
+      await contractIstance.setVote(1);
+      await contractIstance.endVotingSession();
 
-    await contractIstance.startVotingSession();
-
-    await expectRevert(contractIstance.setVote(42), "Proposal not found");
-
-    let proposal = await contractIstance.getOneProposal(1);
-    expect(proposal.voteCount).to.be.bignumber.equal(zero);
-
-    const tx = await contractIstance.setVote(1);
-
-    proposal = await contractIstance.getOneProposal(1);
-    expect(proposal.voteCount).to.be.bignumber.equal(one);
-
-    await expectEvent(tx, "Voted");
-  });
-
-  it("should change work flow to start proposals registering", async () => {
-    await contractIstance.addVoter(owner);
-    await expectRevert(
-      contractIstance.startProposalsRegistering({
-        from: account1,
-      }),
-      notOwner
-    );
-
-    const tx = await contractIstance.startProposalsRegistering();
-
-    await expectRevert(
-      contractIstance.startProposalsRegistering(),
-      "Registering proposals cant be started now"
-    );
-
-    expectEvent(tx, "WorkflowStatusChange");
-
-    const proposal = await contractIstance.getOneProposal(0);
-
-    expect(proposal.description).to.equal("GENESIS");
-  });
-
-  it("should change work flow to end proposals registering", async () => {
-    await expectRevert(
-      contractIstance.endProposalsRegistering({
-        from: account1,
-      }),
-      notOwner
-    );
-
-    await expectRevert(
-      contractIstance.endProposalsRegistering(),
-      "Registering proposals havent started yet"
-    );
-
-    await contractIstance.startProposalsRegistering();
-    const tx = await contractIstance.endProposalsRegistering();
-
-    expectEvent(tx, "WorkflowStatusChange");
-  });
-
-  it("should change work flow to start voting session", async () => {
-    await expectRevert(
-      contractIstance.startVotingSession({
-        from: account1,
-      }),
-      notOwner
-    );
-
-    await expectRevert(
-      contractIstance.startVotingSession(),
-      "Registering proposals phase is not finished."
-    );
-
-    await contractIstance.startProposalsRegistering();
-    await contractIstance.endProposalsRegistering();
-
-    const tx = await contractIstance.startVotingSession();
-    expectEvent(tx, "WorkflowStatusChange");
-  });
-
-  it("should change work flow to end voting session", async () => {
-    await expectRevert(
-      contractIstance.endVotingSession({
-        from: account1,
-      }),
-      notOwner
-    );
-
-    await expectRevert(
-      contractIstance.endVotingSession(),
-      VotingSessionNotStarted
-    );
-
-    await contractIstance.startProposalsRegistering();
-    await contractIstance.endProposalsRegistering();
-    await contractIstance.startVotingSession();
-
-    const tx = await contractIstance.endVotingSession();
-    expectEvent(tx, "WorkflowStatusChange");
-  });
-
-  it("should tally votes", async () => {
-    await expectRevert(
-      contractIstance.tallyVotes({ from: account1 }),
-      notOwner
-    );
-
-    await expectRevert(
-      contractIstance.tallyVotes(),
-      "Current status is not voting session ended"
-    );
-
-    let winner = (await contractIstance.winningProposalID.call()).words[0];
-    expect(new BN(winner)).to.be.bignumber.equal(zero);
-
-    await contractIstance.addVoter(owner);
-    await contractIstance.startProposalsRegistering();
-    await contractIstance.addProposal(desc);
-    await contractIstance.endProposalsRegistering();
-    await contractIstance.startVotingSession();
-    await contractIstance.setVote(1);
-    await contractIstance.endVotingSession();
-
-    const tx = await contractIstance.tallyVotes();
-    await expectEvent(tx, "WorkflowStatusChange");
-    winner = (await contractIstance.winningProposalID.call()).words[0];
-    expect(new BN(winner)).to.be.bignumber.equal(one);
+      const tx = await contractIstance.tallyVotes();
+      await expectEvent(tx, "WorkflowStatusChange");
+      winner = (await contractIstance.winningProposalID.call()).words[0];
+      expect(BN(winner)).to.be.bignumber.equal(one);
+    });
   });
 });
